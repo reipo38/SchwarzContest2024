b80e3f144200d39a4d478814dd9466203009e55f
reipo38 <145777265+reipo38@users.noreply.github.com>
2024-05-02 16:59:20 +0300

    Update train_tickets_system.rs

--- train_tickets_system.rs
+++ train_tickets_system.rs
@@ -1,9 +1,8 @@
 use std::collections::HashMap;
 use std::io;
-
 fn calculate_discount(age: u32, time: u32, price: &f64) -> Result<f64, &str> {
     let mut discount = 0.0;
-    if (time >= 0 && time < 7) || (time >= 19 && time <= 23) {
+    if time < 7 || (time >= 19 && time <= 23) {
         discount += 0.05;
     }
     if age >= 7 && age < 12 {
@@ -13,16 +12,18 @@ fn calculate_discount(age: u32, time: u32, price: &f64) -> Result<f64, &str> {
     }
     if age > 6 { Ok(discount * price) } else { Err("") }
 }
-
 fn calculate_profit(price: &f64) -> f64 {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
     let mut i = 0;
     let mut profit = 0.0;
-    let mut input_clone = input.clone().trim().to_string();
-    while input_clone != "NEXT DESTINATION".to_string() {
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim().to_string();
+        if input == "NEXT DESTINATION" {
+            break;
+        }
         let age: u32 = input[0..input.find('y').unwrap()].parse().unwrap();
-        let time: u32 = input[input.len() - 6..input.len() - 4].parse().unwrap();
+        let time: u32 = input[input.len() - 5..input.len() - 3].parse().unwrap();
         let discount = calculate_discount(age, time, &price);
         match discount {
             Ok(discount) => {
@@ -31,9 +32,6 @@ fn calculate_profit(price: &f64) -> f64 {
             }
             Err(_) => (),
         }
-        input.clear();
-        io::stdin().read_line(&mut input).unwrap();
-        input_clone = input.clone().trim().to_string();
     }
     if i >= 4 {
         profit -= price * 0.05 * i as f64;
@@ -47,25 +45,20 @@ fn main() {
     let n = input.trim().parse().unwrap();
     let mut prices = HashMap::new();
     let mut profits = HashMap::new();
-
     for _ in 0..n {
-        input.clear();
+        let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-
-        let input_clone = input.clone();
-        let trimmed_input = input_clone.trim().to_string();
-        let trimmed_input = trimmed_input[..trimmed_input.len() - 3].to_string();
-
-        let data: Vec<String> = trimmed_input.split(" ").map(|s| s.to_string()).collect();
+        let input = input.trim()[..input.len() - 4].to_string();
+        let data: Vec<String> = input.split(" ").map(|s| s.to_string()).collect();
         let price: f64 = data[1].parse().unwrap();
         prices.insert(data[0].to_string(), price);
     }
-    for (k, v) in &prices {
+    for (k, v) in prices {
         println!("{}:", k);
         let total_price = calculate_profit(&v);
         profits.insert(k, total_price);
     }
-    for (k, v) in &profits {
+    for (k, v) in profits {
         println!("{} : {}", k, v);
     }
 }
